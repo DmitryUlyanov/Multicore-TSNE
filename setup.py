55eeb9917eba4580bbee8b0a9f6415a7293e9ff6
Kernc <kerncece@gmail.com>
2017-10-13 18:55:48 +0200

    setup.py: make more compatible to install/develop

--- setup.py
+++ setup.py
@@ -1,54 +1,72 @@
 import sys
+import shutil
 import os
-from setuptools.command.install import install
-from setuptools import setup
+from os import path
+from subprocess import call as execute
 
-'''
-    Just because I wanted .so file to be built same way for python and torch
-    we exec cmake from cmd here.
-'''
+from setuptools.command.build_ext import build_ext
+from setuptools import setup, find_packages, Extension
 
 
-class MyInstall(install):
+PACKAGE_NAME = "MulticoreTSNE"
+
+VERSION = '0.1'
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
     def run(self):
-        if not os.path.exists('multicore_tsne/release'):
-            os.makedirs('multicore_tsne/release')
-        else:
-            os.system('rm -rf multicore_tsne/release/')
-            os.makedirs('multicore_tsne/release')
+        if 0 != os.system('cmake --version'):
+            sys.exit('\nError: Cannot find cmake. Install cmake, e.g. `pip install cmake`.')
+        for ext in self.extensions:
+            self.build_extension(ext)
 
-        os.chdir('multicore_tsne/release/')
-        return_val = os.system('cmake -DCMAKE_BUILD_TYPE=RELEASE ..')
+    def build_extension(self, ext):
+        SOURCE_DIR = ext.sourcedir
+        EXT_DIR = path.abspath(path.dirname(self.get_ext_fullpath(ext.name)))
+        BUILD_TEMP = self.build_temp
 
-        if return_val != 0:
-            print('cannot find cmake')
-            exit(-1)
+        shutil.rmtree(BUILD_TEMP, ignore_errors=True)
+        os.makedirs(BUILD_TEMP)
 
-        os.system('make VERBOSE=1')
-        os.chdir('../..')
-        print(os.getcwd())
-        os.system(
-            'cp multicore_tsne/release/libtsne_multicore.so python/libtsne_multicore.so')
-        install.run(self)
+        # Run cmake
+        if 0 != execute(['cmake',
+                         '-DCMAKE_BUILD_TYPE={}'.format('Debug' if self.debug else 'Release'),
+                         '-DCMAKE_VERBOSE_MAKEFILE={}'.format(int(self.verbose)),
+                         "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY='{}'".format(EXT_DIR),
+                         SOURCE_DIR], cwd=BUILD_TEMP):
+            sys.exit('\nERROR: Cannot generate Makefile. See above errors.')
+
+        # Run make
+        if 0 != execute('cmake --build . -- -j4', shell=True, cwd=BUILD_TEMP):
+            sys.exit('\nERROR: Cannot find make? See above errors.')
 
 
-setup(
-    name="MulticoreTSNE",
-    version="0.1",
-    description='Multicore version of t-SNE algorithm.',
-    author="Dmitry Ulyanov (based on L. Van der Maaten's code)",
-    author_email='dmitry.ulyanov.msu@gmail.com',
-    url='https://github.com/DmitryUlyanov/Multicore-TSNE',
-    install_requires=[
-        'numpy',
-        'psutil',
-        'cffi'
-    ],
+if __name__ == '__main__':
+    EXT_MODULES = []
+    if 'test' not in sys.argv:
+        EXT_MODULES = [CMakeExtension('MulticoreTSNE.MulticoreTSNE',
+                                      sourcedir='multicore_tsne')]
+    setup(
+        name=PACKAGE_NAME,
+        version=VERSION,
+        description='Multicore version of t-SNE algorithm.',
+        author="Dmitry Ulyanov (based on L. Van der Maaten's code)",
+        author_email='dmitry.ulyanov.msu@gmail.com',
+        url='https://github.com/DmitryUlyanov/Multicore-TSNE',
+        install_requires=[
+            'numpy',
+            'psutil',
+            'cffi'
+        ],
+        packages=find_packages(),
+        include_package_data=True,
 
-    packages=['MulticoreTSNE'],
-    package_dir={'MulticoreTSNE': 'python'},
-    package_data={'MulticoreTSNE': ['multicore_tsne.so']},
-    include_package_data=True,
-
-    cmdclass={"install": MyInstall},
-)
+        ext_modules=EXT_MODULES,
+        cmdclass={'build_ext': CMakeBuild},
+    )
