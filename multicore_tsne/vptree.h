930fef27fa6f7035f8ac97a9fb5f764dc75ec45b
Dmitry Ulyanov <dmitry.ulyanov.msu@gmail.com>
2017-11-20 22:04:24 +0300

    fix euclidean_distance

--- multicore_tsne/vptree.h
+++ multicore_tsne/vptree.h
@@ -75,9 +75,10 @@ public:
 double euclidean_distance(const DataPoint &t1, const DataPoint &t2) {
     double dd = .0;
     for (int d = 0; d < t1.dimensionality(); d++) {
-        dd += (t1.x(d) - t2.x(d)) * (t1.x(d) - t2.x(d));
+        double t = (t1.x(d) - t2.x(d));
+        dd += t * t;
     }
-    return dd;
+    return sqrt(dd);
 }
 
 
