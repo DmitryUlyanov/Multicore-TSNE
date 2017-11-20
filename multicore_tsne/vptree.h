4f1b742453534e1a955472f2bf46a8bcb7519f9a
Dmitry Ulyanov <dmitry.ulyanov.msu@gmail.com>
2017-11-20 22:09:41 +0300

    remove copy

--- multicore_tsne/vptree.h
+++ multicore_tsne/vptree.h
@@ -24,9 +24,10 @@ class DataPoint
 {
     int _D;
     int _ind;
-    double* _x;
+    
 
 public:
+    double* _x;
     DataPoint() {
         _D = 1;
         _ind = -1;
@@ -35,34 +36,37 @@ public:
     DataPoint(int D, int ind, double* x) {
         _D = D;
         _ind = ind;
-        _x = (double*) malloc(_D * sizeof(double));
-        for (int d = 0; d < _D; d++) {
-            _x[d] = x[d];
-        }
+        _x = x;
+        // _x = (double*) malloc(_D * sizeof(double));
+        // for (int d = 0; d < _D; d++) {
+        //     _x[d] = x[d];
+        // }
     }
     DataPoint(const DataPoint& other) {                     // this makes a deep copy -- should not free anything
         if (this != &other) {
             _D = other.dimensionality();
             _ind = other.index();
-            _x = (double*) malloc(_D * sizeof(double));
-            for (int d = 0; d < _D; d++) {
-                _x[d] = other.x(d);
-            }
+            // _x = (double*) malloc(_D * sizeof(double));
+            // for (int d = 0; d < _D; d++) {
+            //     _x[d] = other.x(d);
+            // }
+            _x = other._x;
         }
     }
     ~DataPoint() { 
-        if (_x != NULL) 
-            free(_x); 
+        // if (_x != NULL) 
+        //     free(_x); 
     }
     DataPoint& operator= (const DataPoint& other) {         // asignment should free old object
         if (this != &other) {
-            if (_x != NULL) free(_x);
+            // if (_x != NULL) free(_x);
             _D = other.dimensionality();
             _ind = other.index();
-            _x = (double*) malloc(_D * sizeof(double));
-            for (int d = 0; d < _D; d++) {
-                _x[d] = other.x(d);
-            }
+            // _x = (double*) malloc(_D * sizeof(double));
+            // for (int d = 0; d < _D; d++) {
+            //     _x[d] = other.x(d);
+            // }
+            _x = other._x;
         }
         return *this;
     }
